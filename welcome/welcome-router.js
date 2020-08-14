a4398ba5d66c7a922dfdb378704fd59dd507dfc2
Jose Paquian <alostnight1993@outlook.com>
2020-08-13 21:11:04 -0500

    deploy

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -3,8 +3,8 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-	res.json({
-		message: `Welcome to ${process.env.API}, ${process.env.NEW}`,
+	res.status(200).json({
+		message: `Welcome to ${process.env.API}, ${process.env.NAME}`,
 	})
 })
 
